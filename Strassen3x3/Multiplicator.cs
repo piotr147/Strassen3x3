b9badb9568b06e6f53aaf3e3838b2fb9819e4d78
Piotr Kośkiewicz <piotr888k@gmail.com>
2021-04-22 09:04:48 +0200

    Separate abstractions

--- Strassen3x3/Multiplicator.cs
+++ Strassen3x3/Multiplicator.cs
@@ -1,45 +1,35 @@
-﻿namespace Strassen3x3
+﻿using System;
+
+namespace Strassen3x3
 {
     public static class Multiplicator
     {
+        private const int STRASSEN_SIZE = 3;
+
         public static int[,] Multiplicate(int[,] A, int[,] B)
         {
             if (A.Length == 1)
-                return new int[1, 1] { { A[0, 0] * B[0, 0] } };
+                return CalculateOneElementResult(A, B);
 
-            int[,][,] a = new int[,][,]
-            {
-                {null, null, null, null },
-                { null, GenerateMatrix(A, 1, 2), GenerateMatrix(A, 1, 2), GenerateMatrix(A, 1, 3) },
-                { null, GenerateMatrix(A, 2, 1), GenerateMatrix(A, 2, 2), GenerateMatrix(A, 2, 3) },
-                { null, GenerateMatrix(A, 3, 1), GenerateMatrix(A, 3, 2), GenerateMatrix(A, 3, 3) }
-            };
+            int[,][,] a = GenerateBlockMatricesFromInput(A);
+            int[,][,] b = GenerateBlockMatricesFromInput(B);
 
-            int[,][,] b = new int[,][,]
-            {
-                {null, null, null, null },
-                { null, GenerateMatrix(B, 1, 1), GenerateMatrix(B, 1, 2), GenerateMatrix(B, 1, 3) },
-                { null, GenerateMatrix(B, 2, 1), GenerateMatrix(B, 2, 2), GenerateMatrix(B, 2, 3) },
-                { null, GenerateMatrix(B, 3, 1), GenerateMatrix(B, 3, 2), GenerateMatrix(B, 3, 3) }
-            };
+            int[][,] m = GenerateMatricesM(a, b);
 
-            int[][,] m = GenerateMatrixM(a, b);
-
-            int[,][,] c = new int[4, 4][,];
-
-            c[1, 1] = m[6].Plus(m[14]).Plus(m[19]);
-            // ...
-            c[3, 3] = m[6].Plus(m[7]).Plus(m[8]).Plus(m[9]).Plus(m[23]);
+            int[,][,] c = GenerateBlockMatricesForC(m);
 
             return MergeToOneMatrix(c);
         }
 
-        private static int[,] GenerateMatrix(int[,] m, int v1, int v2)
+        private static int[,] CalculateOneElementResult(int[,] a, int[,] b) =>
+            new int[1, 1] { { a[0, 0] * b[0, 0] } };
+
+        private static int[,] GenerateMatrix(int[,] m, int ind1, int ind2)
         {
-            int resultLength = m.GetLength(0) / 3;
+            int resultLength = m.GetLength(0) / STRASSEN_SIZE;
             int[,] result = new int[resultLength, resultLength];
-            int startIndex1 = (v1 - 1) * resultLength;
-            int startIndex2 = (v2 - 1) * resultLength;
+            int startIndex1 = (ind1 - 1) * resultLength;
+            int startIndex2 = (ind2 - 1) * resultLength;
 
             for (int i = 0; i < resultLength; ++i)
             {
@@ -52,7 +42,16 @@
             return result;
         }
 
-        private static int[][,] GenerateMatrixM(int[,][,] a, int[,][,] b)
+        private static int[,][,] GenerateBlockMatricesFromInput(int[,] m) =>
+            new int[,][,]
+            {
+                {null, null, null, null },
+                { null, GenerateMatrix(m, 1, 2), GenerateMatrix(m, 1, 2), GenerateMatrix(m, 1, 3) },
+                { null, GenerateMatrix(m, 2, 1), GenerateMatrix(m, 2, 2), GenerateMatrix(m, 2, 3) },
+                { null, GenerateMatrix(m, 3, 1), GenerateMatrix(m, 3, 2), GenerateMatrix(m, 3, 3) }
+            };
+
+        private static int[][,] GenerateMatricesM(int[,][,] a, int[,][,] b)
         {
             int[][,] m = new int[24][,];
 
@@ -66,10 +65,21 @@
             return m;
         }
 
+        private static int[,][,] GenerateBlockMatricesForC(int[][,] m)
+        {
+            int[,][,] c = new int[STRASSEN_SIZE + 1, STRASSEN_SIZE + 1][,];
+
+            c[1, 1] = m[6].Plus(m[14]).Plus(m[19]);
+            // ...
+            c[3, 3] = m[6].Plus(m[7]).Plus(m[8]).Plus(m[9]).Plus(m[23]);
+
+            return c;
+        }
+
         private static int[,] MergeToOneMatrix(int[,][,] c)
         {
             int cLength = c[1, 1].GetLength(0);
-            int[,] result = new int[cLength * 3, cLength * 3];
+            int[,] result = new int[cLength * STRASSEN_SIZE, cLength * STRASSEN_SIZE];
 
             for (int i = 1; i < c.GetLength(0); ++i)
             {
