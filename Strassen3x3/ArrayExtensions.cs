92f44f4f59afcdd3a084d941ac085b52c5993277
Piotr Kośkiewicz <piotr888k@gmail.com>
2021-04-29 22:39:06 +0200

    Algorithm works

--- Strassen3x3/ArrayExtensions.cs
+++ Strassen3x3/ArrayExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Strassen3x3
+﻿using System;
+using System.Text;
+
+namespace Strassen3x3
 {
     public static class ArrayExtensions
     {
@@ -25,7 +28,7 @@
             {
                 for (int j = 0; j < result.GetLength(1); ++j)
                 {
-                    result[i, j] = m1[i, j] + m2[i, j];
+                    result[i, j] = m1[i, j] - m2[i, j];
                 }
             }
 
@@ -45,6 +48,61 @@
             }
 
             return result;
+        }
+
+        public static void Print(this int[,] m1)
+        {
+            int width = FindWidth(m1);
+
+            for (int i = 0; i < m1.GetLength(0); ++i)
+            {
+                for (int j = 0; j < m1.GetLength(1); ++j)
+                {
+                    PrintOnCertainWidth(m1[i, j], width);
+                }
+
+                Console.Write("\n");
+            }
+        }
+
+        private static int FindWidth(int[,] m1)
+        {
+            int maxDigitsNumber = 1;
+
+            for (int i = 0; i < m1.GetLength(0); ++i)
+            {
+                for (int j = 0; j < m1.GetLength(1); ++j)
+                {
+                    int currentDigitsNumber = FindNumberOfDigits(m1[i, j]);
+                    if (currentDigitsNumber > maxDigitsNumber)
+                    {
+                        maxDigitsNumber = currentDigitsNumber;
+                    }
+                }
+            }
+
+            return maxDigitsNumber + 1;
+        }
+
+        private static void PrintOnCertainWidth(int n, int width)
+        {
+            Console.Write($"{n}");
+            Console.Write($"{NSpaces(width - FindNumberOfDigits(n))}");
+        }
+
+        private static int FindNumberOfDigits(int n) =>
+            (int)Math.Ceiling(Math.Log(n, 10)) + (n < 0 ? 1 : 0);
+
+        private static string NSpaces(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < n; ++i)
+            {
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
         }
     }
 }
--- Strassen3x3/Multiplicator.cs
+++ Strassen3x3/Multiplicator.cs
@@ -1,35 +1,51 @@
-﻿using System;
-
-namespace Strassen3x3
+﻿namespace Strassen3x3
 {
     public static class Multiplicator
     {
-        private const int STRASSEN_SIZE = 3;
-
         public static int[,] Multiplicate(int[,] A, int[,] B)
         {
             if (A.Length == 1)
-                return CalculateOneElementResult(A, B);
+                return new int[1, 1] { { A[0, 0] * B[0, 0] } };
 
-            int[,][,] a = GenerateBlockMatricesFromInput(A);
-            int[,][,] b = GenerateBlockMatricesFromInput(B);
+            int[,][,] a = new int[,][,]
+            {
+                {null, null, null, null },
+                { null, GenerateMatrix(A, 1, 1), GenerateMatrix(A, 1, 2), GenerateMatrix(A, 1, 3) },
+                { null, GenerateMatrix(A, 2, 1), GenerateMatrix(A, 2, 2), GenerateMatrix(A, 2, 3) },
+                { null, GenerateMatrix(A, 3, 1), GenerateMatrix(A, 3, 2), GenerateMatrix(A, 3, 3) }
+            };
 
-            int[][,] m = GenerateMatricesM(a, b);
+            int[,][,] b = new int[,][,]
+            {
+                {null, null, null, null },
+                { null, GenerateMatrix(B, 1, 1), GenerateMatrix(B, 1, 2), GenerateMatrix(B, 1, 3) },
+                { null, GenerateMatrix(B, 2, 1), GenerateMatrix(B, 2, 2), GenerateMatrix(B, 2, 3) },
+                { null, GenerateMatrix(B, 3, 1), GenerateMatrix(B, 3, 2), GenerateMatrix(B, 3, 3) }
+            };
 
-            int[,][,] c = GenerateBlockMatricesForC(m);
+            int[][,] m = GenerateMatrixM(a, b);
+
+            int[,][,] c = new int[4, 4][,];
+
+            c[1, 1] = m[6].Plus(m[14]).Plus(m[19]);
+            c[1, 2] = m[1].Plus(m[4]).Plus(m[5]).Plus(m[6]).Plus(m[12]).Plus(m[14]).Plus(m[15]);
+            c[1, 3] = m[6].Plus(m[7]).Plus(m[9]).Plus(m[10]).Plus(m[14]).Plus(m[16]).Plus(m[18]);
+            c[2, 1] = m[2].Plus(m[3]).Plus(m[4]).Plus(m[6]).Plus(m[14]).Plus(m[16]).Plus(m[17]);
+            c[2, 2] = m[2].Plus(m[4]).Plus(m[5]).Plus(m[6]).Plus(m[20]);
+            c[2, 3] = m[14].Plus(m[16]).Plus(m[17]).Plus(m[18]).Plus(m[21]);
+            c[3, 1] = m[6].Plus(m[7]).Plus(m[8]).Plus(m[11]).Plus(m[12]).Plus(m[13]).Plus(m[14]);
+            c[3, 2] = m[12].Plus(m[13]).Plus(m[14]).Plus(m[15]).Plus(m[22]);
+            c[3, 3] = m[6].Plus(m[7]).Plus(m[8]).Plus(m[9]).Plus(m[23]);
 
             return MergeToOneMatrix(c);
         }
 
-        private static int[,] CalculateOneElementResult(int[,] a, int[,] b) =>
-            new int[1, 1] { { a[0, 0] * b[0, 0] } };
-
-        private static int[,] GenerateMatrix(int[,] m, int ind1, int ind2)
+        private static int[,] GenerateMatrix(int[,] m, int v1, int v2)
         {
-            int resultLength = m.GetLength(0) / STRASSEN_SIZE;
+            int resultLength = m.GetLength(0) / 3;
             int[,] result = new int[resultLength, resultLength];
-            int startIndex1 = (ind1 - 1) * resultLength;
-            int startIndex2 = (ind2 - 1) * resultLength;
+            int startIndex1 = (v1 - 1) * resultLength;
+            int startIndex2 = (v2 - 1) * resultLength;
 
             for (int i = 0; i < resultLength; ++i)
             {
@@ -42,48 +58,88 @@ namespace Strassen3x3
             return result;
         }
 
-        private static int[,][,] GenerateBlockMatricesFromInput(int[,] m) =>
-            new int[,][,]
-            {
-                {null, null, null, null },
-                { null, GenerateMatrix(m, 1, 2), GenerateMatrix(m, 1, 2), GenerateMatrix(m, 1, 3) },
-                { null, GenerateMatrix(m, 2, 1), GenerateMatrix(m, 2, 2), GenerateMatrix(m, 2, 3) },
-                { null, GenerateMatrix(m, 3, 1), GenerateMatrix(m, 3, 2), GenerateMatrix(m, 3, 3) }
-            };
-
-        private static int[][,] GenerateMatricesM(int[,][,] a, int[,][,] b)
+        private static int[][,] GenerateMatrixM(int[,][,] a, int[,][,] b)
         {
             int[][,] m = new int[24][,];
 
             m[0] = null;
             m[1] = Multiplicate(
                 a[1, 1].Plus(a[1, 2]).Plus(a[1, 3]).Minus(a[2, 1]).Minus(a[2, 2]).Minus(a[3, 2]).Minus(a[3, 3]),
-                b[3, 3]);
-            // ...
+                b[2, 2]);
+            m[2] = Multiplicate(
+                a[1, 1].Minus(a[2, 1]),
+                b[1, 2].Minus().Plus(b[2, 2]));
+            m[3] = Multiplicate(
+                a[2, 2],
+                b[1, 1].Minus().Plus(b[1, 2]).Plus(b[2, 1]).Minus(b[2, 2]).Minus(b[2, 3]).Minus(b[3, 1]).Plus(b[3, 3]));
             m[4] = Multiplicate(
                 a[1, 1].Minus().Plus(a[2, 1]).Plus(a[2, 2]),
                 b[1, 1].Minus(b[1, 2]).Plus(b[2, 2]));
-            // ...
-            m[23] = Multiplicate(a[3, 3], b[3, 3]);
+            m[5] = Multiplicate(
+                a[2, 1].Plus(a[2, 2]),
+                b[1, 1].Minus().Plus(b[1, 2]));
+            m[6] = Multiplicate(
+                a[1, 1],
+                b[1, 1]);
+            m[7] = Multiplicate(
+                a[1, 1].Minus().Plus(a[3, 1]).Plus(a[3, 2]),
+                b[1, 1].Minus(b[1, 3]).Plus(b[2, 3]));
+            m[8] = Multiplicate(
+                a[1, 1].Minus().Plus(a[3, 1]),
+                b[1, 3].Minus(b[2, 3]));
+            m[9] = Multiplicate(
+                a[3, 1].Plus(a[3, 2]),
+                b[1, 1].Minus().Plus(b[1, 3]));
+            m[10] = Multiplicate(
+                a[1, 1].Plus(a[1, 2]).Plus(a[1, 3]).Minus(a[2, 2]).Minus(a[2, 3]).Minus(a[3, 1]).Minus(a[3, 2]),
+                b[2, 3]);
+            m[11] = Multiplicate(
+                a[3, 2],
+                b[1, 1].Minus().Plus(b[1, 3]).Plus(b[2, 1]).Minus(b[2, 2]).Minus(b[2, 3]).Minus(b[3, 1]).Plus(b[3, 2]));
+            m[12] = Multiplicate(
+                a[1, 3].Minus().Plus(a[3, 2]).Plus(a[3, 3]),
+                b[2, 2].Plus(b[3, 1]).Minus(b[3, 2]));
+            m[13] = Multiplicate(
+                a[1, 3].Minus(a[3, 3]),
+                b[2, 2].Minus(b[3, 2]));
+            m[14] = Multiplicate(
+                a[1, 3],
+                b[3, 1]);
+            m[15] = Multiplicate(
+                a[3, 2].Plus(a[3, 3]),
+                b[3, 1].Minus().Plus(b[3, 2]));
+            m[16] = Multiplicate(
+                a[1, 3].Minus().Plus(a[2, 2]).Plus(a[2, 3]),
+                b[2, 3].Plus(b[3, 1]).Minus(b[3, 3]));
+            m[17] = Multiplicate(
+                a[1, 3].Minus(a[2, 3]),
+                b[2, 3].Minus(b[3, 3]));
+            m[18] = Multiplicate(
+                a[2, 2].Plus(a[2, 3]),
+                b[3, 1].Minus().Plus(b[3, 3]));
+            m[19] = Multiplicate(
+                a[1, 2],
+                b[2, 1]);
+            m[20] = Multiplicate(
+                a[2, 3],
+                b[3, 2]);
+            m[21] = Multiplicate(
+                a[2, 1],
+                b[1, 3]);
+            m[22] = Multiplicate(
+                a[3, 1],
+                b[1, 2]);
+            m[23] = Multiplicate(
+                a[3, 3],
+                b[3, 3]);
 
             return m;
-        }
-
-        private static int[,][,] GenerateBlockMatricesForC(int[][,] m)
-        {
-            int[,][,] c = new int[STRASSEN_SIZE + 1, STRASSEN_SIZE + 1][,];
-
-            c[1, 1] = m[6].Plus(m[14]).Plus(m[19]);
-            // ...
-            c[3, 3] = m[6].Plus(m[7]).Plus(m[8]).Plus(m[9]).Plus(m[23]);
-
-            return c;
         }
 
         private static int[,] MergeToOneMatrix(int[,][,] c)
         {
             int cLength = c[1, 1].GetLength(0);
-            int[,] result = new int[cLength * STRASSEN_SIZE, cLength * STRASSEN_SIZE];
+            int[,] result = new int[cLength * 3, cLength * 3];
 
             for (int i = 1; i < c.GetLength(0); ++i)
             {
